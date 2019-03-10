76f0f9b6e244a9fbd956ab77adf0a1ecd317372a
Maralex <alextron98@gmail.com>
2019-03-10 14:38:08 +0200

    test

---
+++ t1/src/main/java/com/marAlex/t1/App.java
@@ -0,0 +1,13 @@
+package com.marAlex.t1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
